aee28ba1126a7c2c0e69671428d1a3a23534d868
Ashutoshkumar75 <ashutoshkumar75mgr@gmail.com>
2020-12-23 16:31:05 +0530

    banner-update

--- src/components/navigationFullScreen.js
+++ src/components/navigationFullScreen.js
@@ -31,7 +31,7 @@ const NavigationFullScreen = ({ toggleMenu, setToggleMenu }) => {
     {
       id: 3,
       link: "CONTACT US",
-      path: "/contsct-us",
+      path: "/contactUs",
     },
   ]
   const navSocials = [
